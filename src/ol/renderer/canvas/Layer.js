dbe9529a05ed65940763d0895b365dd91b7d33e1
Gurzixo <gurzixo@platinn.commail>
2025-11-26 15:55:27 +0000

    reverted

--- src/ol/renderer/canvas/Layer.js
+++ src/ol/renderer/canvas/Layer.js
@@ -175,7 +175,7 @@ class CanvasLayerRenderer extends LayerRenderer {
     ) {
       const canvas = target.firstElementChild;
       if (canvas instanceof HTMLCanvasElement) {
-        context = canvas.getContext('2d', {willReadFrequently: true});
+        context = canvas.getContext('2d');
       }
     }
     if (context && context.canvas.style.transform === transform) {
